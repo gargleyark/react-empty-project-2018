574521c4be0a1dc2d1318642aaace9badb5bf606
gargleyark <myself@gargleyark.com>
2018-12-11 00:07:14 +0000

    adds products async call

--- src/app-router/App.jsx
+++ src/app-router/App.jsx
@@ -7,6 +7,19 @@ import Header from '../components/Header/Header.jsx';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://raw.githubusercontent.com/gargleyark/react-empty-project-2018/react-routing-with-link/src/data/products.json'
+    )
+      .then(res => res.json())
+      .then(data => this.setState({ products: data }));
+  }
+
   render() {
     return (
       <BrowserRouter>
