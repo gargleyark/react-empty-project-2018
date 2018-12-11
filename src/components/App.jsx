8ebb50dafe0eb177a9b58e3fcc84a6148f83d7c2
gargleyark <myself@gargleyark.com>
2018-12-11 00:20:18 +0000

    adds constructor to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <div>Hello</div>;
   }
